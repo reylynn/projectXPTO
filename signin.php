607e5bee44805b61702c2c0edb0889cac15a3ea4
reyreeeeeeey <reginaldo.reylynn@gmail.com>
2017-07-18 10:35:54 +0800

    Add signin.php

---
+++ signin.php
@@ -0,0 +1,32 @@
+<?php
+session_start(); // Starting Session
+$error=''; // Variable To Store Error Message
+if (isset($_POST['submit'])) {
+if (empty($_POST['username']) || empty($_POST['password'])) {
+$error = "Username or Password is invalid";
+}
+else
+{
+// Define $username and $password
+$username=$_POST['username'];
+$password=$_POST['password'];
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+$connection = mysql_connect("localhost", "root", "123456789");
+// To protect MySQL injection for Security purpose
+$username = mysql_real_escape_string($username);
+$password = mysql_real_escape_string($password);
+// Selecting Database
+$db = mysql_select_db("userAccount", $connection);
+// SQL query to fetch information of registerd users and finds user match.
+$query = mysql_query("SELECT * FROM user WHERE password='$password' AND username='$username'", $connection);
+$rows = mysql_num_rows($query);
+if ($rows == 1) {
+$_SESSION['login_user']=$username; // Initializing Session
+header("location: profile.php"); // Redirecting To Other Page
+} else {
+$error = "Username or Password is invalid";
+}
+mysql_close($connection); // Closing Connection
+}
+}
+?>
